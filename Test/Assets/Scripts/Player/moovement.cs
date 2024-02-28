f4c5f92f691398faf1e4c78551a713b5cf37b6cf
Herst0 <theo.larbre@epita.fr>
2024-02-28 17:22:09 +0100

    Animation
    Encore des tests

--- Test/Assets/Scripts/Player/moovement.cs
+++ Test/Assets/Scripts/Player/moovement.cs
@@ -6,20 +6,20 @@ public class moove : MonoBehaviour
 {
     public float speed = 6f;
     public float gravity = 20f;
-
     private Vector3 moveD = Vector3.zero;
-
+    
+    private Animator animator;
+    private bool isWalking = false;
     CharacterController Cac;
 
     [SerializeField]
     private float forceMagnitude;
-    
     // Start is called before the first frame update
     void Start()
     {
         Cac = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -34,9 +34,19 @@ public class moove : MonoBehaviour
         moveD.y -= gravity * Time.deltaTime;
 
         Cac.Move(moveD * Time.deltaTime);
+        
+        
+        if (Input.GetKey(KeyCode.Z))
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+        
+        animator.SetBool("IsWalking", isWalking);
     }
-
-
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         var rigidBody = hit.collider.attachedRigidbody;
