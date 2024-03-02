94ec9fdc5a7615833c0be192201b3629d5fb5b28
Herst0 <theo.larbre@epita.fr>
2024-03-02 17:49:51 +0100

    Correction de bug

--- Test/Assets/Scripts/Player/ThirdPersonController.cs
+++ Test/Assets/Scripts/Player/ThirdPersonController.cs
@@ -37,7 +37,7 @@ namespace StarterAssets
 
         [Space(10)]
         [Tooltip("The height the player can jump")]
-        public float JumpHeight = 1.2f;
+        public float JumpHeight = 0f;
 
         [Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
         public float Gravity = -15.0f;
@@ -303,18 +303,7 @@ namespace StarterAssets
                     _verticalVelocity = -2f;
                 }
 
-                // Jump
-                if (_input.jump && _jumpTimeoutDelta <= 0.0f)
-                {
-                    // the square root of H * -2 * G = how much velocity needed to reach desired height
-                    _verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
-
-                    // update animator if using character
-                    if (_hasAnimator)
-                    {
-                        _animator.SetBool(_animIDJump, true);
-                    }
-                }
+                // Jump logic removed
 
                 // jump timeout
                 if (_jumpTimeoutDelta >= 0.0f)
@@ -340,9 +329,6 @@ namespace StarterAssets
                         _animator.SetBool(_animIDFreeFall, true);
                     }
                 }
-
-                // if we are not grounded, do not jump
-                _input.jump = false;
             }
 
             // apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
