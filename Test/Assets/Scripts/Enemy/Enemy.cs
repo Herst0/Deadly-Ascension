98486d9d77318330f4565682a1afc178861b8075
Herst0 <theo.larbre@epita.fr>
2024-03-08 23:45:16 +0100

    Ennemies qui bouge !!!

---
+++ Test/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+namespace Enemy
+{
+    public class Enemies : MonoBehaviour
+    {
+        public NavMeshAgent agent; 
+        public Transform player;
+        public LayerMask whatIsground, whatIsplayer;
+        // Patroling
+        public Vector3 walkPoint; 
+        bool walkPointSet;
+        public float walkPointRange; 
+
+        // Attack
+        public float timeBetweenAttacks;
+        private bool alreadyAttacked; 
+
+        // Enemy state
+        public float sightRange, attackRange; 
+        public bool playerInSightRange, playerInAttackRange; 
+        public float health; 
+
+        
+        
+      private void Update()
+        {
+
+            playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsplayer);
+            playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsplayer);
+
+     
+            if (!playerInSightRange && !playerInAttackRange)
+            {
+                Patroling();
+            }
+            if (playerInSightRange && !playerInAttackRange)
+            {
+                ChasePlayer();
+            }
+            if (playerInSightRange && playerInAttackRange)
+            {
+                AttackPlayer();
+            }
+        }
+
+
+        public void Awake()
+        {
+            player = GameObject.Find("Player").transform;
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Patroling()
+        {
+            if (!walkPointSet)
+            {
+                SearchWalkPoint();
+            }
+
+            if (walkPointSet)
+            {
+                agent.SetDestination(walkPoint);
+            }
+
+            Vector3 distanceToWalkPoint = transform.position - walkPoint;
+            if (distanceToWalkPoint.magnitude < 1f)
+            {
+                walkPointSet = false;
+            }
+        }
+
+        private void SearchWalkPoint()
+        {
+            float randomZ = Random.Range(-walkPointRange, walkPointRange);
+            float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+            walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+            if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsground))
+            {
+                walkPointSet = true;
+            }
+        }
+
+        private void ChasePlayer()
+        {
+            agent.SetDestination(player.position);
+        }
+
+        private void AttackPlayer()
+        {
+
+            agent.SetDestination(transform.position);
+
+            transform.LookAt(player);
+
+            if (!alreadyAttacked)
+            {
+                alreadyAttacked = true;
+                StartCoroutine(ResetAttack());
+            }
+        }
+
+        private IEnumerator ResetAttack()
+        {
+            yield return new WaitForSeconds(timeBetweenAttacks);
+            alreadyAttacked = false;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            health = health - damage;
+            if (health <= 0)
+            {
+                StartCoroutine(DestroyEnemy());
+            }
+        }
+
+        private IEnumerator DestroyEnemy()
+        {
+            yield return new WaitForSeconds(0.5f);
+            Destroy(gameObject);
+        }
+    }
+}
