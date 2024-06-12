8259e302e81aff7ee6f46e8881887bdf82b5655a
Herst0 <theo.larbre@epita.fr>
2024-06-12 16:49:27 +0200

    petit changement mult

---
+++ Deadly Ascension(true)/Assets/Scenes/Scripts/Player/PlayerSetup.cs
@@ -0,0 +1,21 @@
+using System;
+using Mirror;
+using UnityEngine;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    private Behaviour[] componentsToDisable;
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+            
+        }
+    }
+}
