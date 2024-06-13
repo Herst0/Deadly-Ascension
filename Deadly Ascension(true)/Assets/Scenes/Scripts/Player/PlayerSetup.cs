62863b74fca02f811b2919e16e2c6fd090d076b4
cryoxe <71180076+cryoxe@users.noreply.github.com>
2024-06-13 16:29:46 +0200

    Commented out for testing purpose

--- Deadly Ascension(true)/Assets/Scenes/Scripts/Player/PlayerSetup.cs
+++ Deadly Ascension(true)/Assets/Scenes/Scripts/Player/PlayerSetup.cs
@@ -9,13 +9,13 @@ public class PlayerSetup : NetworkBehaviour
 
     private void Start()
     {
-        if (!isLocalPlayer)
-        {
-            for (int i = 0; i < componentsToDisable.Length; i++)
-            {
-                componentsToDisable[i].enabled = false;
-            }
-            
-        }
+        // if (!isLocalPlayer)
+        // {
+        //     for (int i = 0; i < componentsToDisable.Length; i++)
+        //     {
+        //         componentsToDisable[i].enabled = false;
+        //     }
+        //     
+        // }
     }
 }
