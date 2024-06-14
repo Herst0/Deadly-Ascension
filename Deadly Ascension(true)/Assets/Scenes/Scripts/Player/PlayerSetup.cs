eca7a88c6460f7a87d5c8970e735aae2fcb98dd5
Brieuc <brieuc.lecras@gmail.com>
2024-06-14 12:50:30 +0200

    push du zombie tireur

--- Deadly Ascension(true)/Assets/Scenes/Scripts/Player/PlayerSetup.cs
+++ Deadly Ascension(true)/Assets/Scenes/Scripts/Player/PlayerSetup.cs
@@ -11,11 +11,30 @@ public class PlayerSetup : NetworkBehaviour
     {
         if (!isLocalPlayer)
         {
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                 componentsToDisable[i].enabled = false;
+            }
+            
+        }
+        else
+        {
+            Camera.main.gameObject.SetActive(false);
+        }
+=======
+>>>>>>> Stashed changes
              for (int i = 0; i < componentsToDisable.Length; i++)
              {
                 componentsToDisable[i].enabled = false;
             }
             
         }
+<<<<<<< Updated upstream
+=======
+>>>>>>> 152a1be6fe9b4796a24c82db8a820e109e3a0b27
+>>>>>>> Stashed changes
     }
 }
