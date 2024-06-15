14bc311923df48ad0cafa770cfa476dace0f8601
Ruben <112523639+rubensoulardgosselin@users.noreply.github.com>
2024-06-16 00:55:32 +0200

    maj spawnonarea
    permet le spawn de différent type de zombies dans une même room

--- Deadly Ascension(true)/Assets/Scenes/Scripts/Spawn/SpawnOnArea.cs
+++ Deadly Ascension(true)/Assets/Scenes/Scripts/Spawn/SpawnOnArea.cs
@@ -9,39 +9,80 @@ public class SpawnZone : MonoBehaviour
 
     [SerializeField]
     private GameObject zomb2;
+    
+    [SerializeField]
+    private GameObject zomb3;
+    
+    [SerializeField]
+    private GameObject zomb4;
+    
+    [SerializeField]
+    private GameObject zomb5;
 
     [SerializeField]
     private Vector3 ZoneSize;
 
 
+
     void Start()
     {
-        int zomb = Random.Range(1,3);
-        int randomIndex = Random.Range(0, 8);
+        
+        int randomIndex = Random.Range(3, 8);
 
-        for (int i = 0; i<= randomIndex; i++)
+        for (int i = 0; i <= randomIndex; i++)
         {
+            int zomb = Random.Range(1, 6);
             if (zomb == 1)
             {
                 GameObject instantiated = Instantiate(zomb1);
-                 instantiated.transform.position = new Vector3(
-                Random.Range(transform.position.x - ZoneSize.x/2, transform.position.x + ZoneSize.x/2),
-                Random.Range(transform.position.y - ZoneSize.y/2, transform.position.y + ZoneSize.y/2),
-                Random.Range(transform.position.z - ZoneSize.z/2, transform.position.z + ZoneSize.z/2)
-            ); 
+                instantiated.transform.position = new Vector3(
+                    Random.Range(transform.position.x - ZoneSize.x / 2, transform.position.x + ZoneSize.x / 2),
+                    Random.Range(transform.position.y - ZoneSize.y / 2, transform.position.y + ZoneSize.y / 2),
+                    Random.Range(transform.position.z - ZoneSize.z / 2, transform.position.z + ZoneSize.z / 2)
+                );
             }
+
             if (zomb == 2)
             {
                 GameObject instantiated = Instantiate(zomb2);
-                 instantiated.transform.position = new Vector3(
-                Random.Range(transform.position.x - ZoneSize.x/2, transform.position.x + ZoneSize.x/2),
-                Random.Range(transform.position.y - ZoneSize.y/2, transform.position.y + ZoneSize.y/2),
-                Random.Range(transform.position.z - ZoneSize.z/2, transform.position.z + ZoneSize.z/2)
-            );
-            } 
+                instantiated.transform.position = new Vector3(
+                    Random.Range(transform.position.x - ZoneSize.x / 2, transform.position.x + ZoneSize.x / 2),
+                    Random.Range(transform.position.y - ZoneSize.y / 2, transform.position.y + ZoneSize.y / 2),
+                    Random.Range(transform.position.z - ZoneSize.z / 2, transform.position.z + ZoneSize.z / 2)
+                );
+            }
+
+            if (zomb == 3)
+            {
+                GameObject instantiated = Instantiate(zomb3);
+                instantiated.transform.position = new Vector3(
+                    Random.Range(transform.position.x - ZoneSize.x / 2, transform.position.x + ZoneSize.x / 2),
+                    Random.Range(transform.position.y - ZoneSize.y / 2, transform.position.y + ZoneSize.y / 2),
+                    Random.Range(transform.position.z - ZoneSize.z / 2, transform.position.z + ZoneSize.z / 2)
+                );
+            }
+            
+            if (zomb == 4)
+            {
+                GameObject instantiated = Instantiate(zomb4);
+                instantiated.transform.position = new Vector3(
+                    Random.Range(transform.position.x - ZoneSize.x / 2, transform.position.x + ZoneSize.x / 2),
+                    Random.Range(transform.position.y - ZoneSize.y / 2, transform.position.y + ZoneSize.y / 2),
+                    Random.Range(transform.position.z - ZoneSize.z / 2, transform.position.z + ZoneSize.z / 2)
+                );
+            }
+            
+            if (zomb == 5)
+            {
+                GameObject instantiated = Instantiate(zomb5);
+                instantiated.transform.position = new Vector3(
+                    Random.Range(transform.position.x - ZoneSize.x / 2, transform.position.x + ZoneSize.x / 2),
+                    Random.Range(transform.position.y - ZoneSize.y / 2, transform.position.y + ZoneSize.y / 2),
+                    Random.Range(transform.position.z - ZoneSize.z / 2, transform.position.z + ZoneSize.z / 2)
+                );
+            }
         }
     }
-
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
