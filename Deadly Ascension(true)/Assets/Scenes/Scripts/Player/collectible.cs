370956fce2712c0187e7cd253a76d49672bc6413
Brieuc <brieuc.lecras@gmail.com>
2024-06-16 06:44:07 +0200

    script tir à compléter

--- Deadly Ascension(true)/Assets/Scenes/Scripts/Player/collectible.cs
+++ Deadly Ascension(true)/Assets/Scenes/Scripts/Player/collectible.cs
@@ -5,24 +5,21 @@ using UnityEngine;
 public class collectible : MonoBehaviour
 {
     private PlayerTakeDamage player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
     
     // Update is called once per frame
     void Update()
     {
         transform.localRotation = Quaternion.Euler(90f, Time.time * 100f, 0);
+        
     }
-
+    
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             player = GameObject.Find("Player").GetComponent<PlayerTakeDamage>();
             player._money += 1;
+            Debug.Log(player._money);
             Destroy(other);
         }
 
