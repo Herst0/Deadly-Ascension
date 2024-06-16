b62c1ebddff43bd8d39664b794ee7f9cc7ec9674
4nt0nym3 <antoine.munoz@epita.fr>
2024-06-16 22:33:30 +0200

    Les gars le UI il marche :D

--- Deadly Ascension(true)/Assets/Scenes/Scripts/Ui/Play screen.cs
+++ Deadly Ascension(true)/Assets/Scenes/Scripts/Ui/Play screen.cs
@@ -3,9 +3,15 @@ using Mirror;
 
 public class MainMenu : MonoBehaviour
 {
-	public void PlayGame()
+	public void PlayGameOffline()
 	{
-		UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+		UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+		//Application.LoadLevel(1);
+	}
+
+	public void PlayGameOnline()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene(3);
 		//Application.LoadLevel(1);
 	}
 
